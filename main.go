c1159799d676936d1d3b8fddbfb9b1d6af98d394
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-17 14:41:29 +0700

    Update take profit and stop loss percentages

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ import (
 
 const (
 	wsURL           = "wss://ws3.indodax.com/ws/"
-	tpPercent       = 0.001 // +0.5%
-	slPercent       = 0.001 // -0.3%
+	tpPercent       = 0.005 // +0.5%
+	slPercent       = 0.003 // -0.3%
 	maxDailyLoss    = 1000.0
 	fixedTradeLimit = 50000.0
 )
