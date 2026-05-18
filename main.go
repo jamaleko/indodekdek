9cdeff02c0c494a0841e03df84e3f30be745ee9e
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-18 07:47:25 +0700

    Update main.go

--- main.go
+++ main.go
@@ -441,7 +441,13 @@ func connectWS() {
 			  14,
 			  candleCloses,
 			 )
-			
+			sendTelegram(fmt.Sprintf(
+				"📊 CHECK\nEMA9: %.0f\nEMA21: %.0f\nEMA50: %.0f\nRSI: %.2f",
+				ema9,
+				ema21,
+				ema50,
+				rsi,
+			))
 			 log.Printf(
 			  "EMA9: %.0f | EMA21: %.0f | EMA50: %.0f | RSI: %.2f",
 			  ema9,
@@ -466,13 +472,6 @@ func connectWS() {
 			 lastReport = time.Now()
 			}
 			if !inPosition {
-				sendTelegram(fmt.Sprintf(
-		"📊 CHECK\nEMA9: %.0f\nEMA21: %.0f\nEMA50: %.0f\nRSI: %.2f",
-		ema9,
-		ema21,
-		ema50,
-		rsi,
-	))
 
 			 if prevEMA9 <= prevEMA21 &&
 			  ema9 > ema21 &&
